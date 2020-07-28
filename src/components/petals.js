9228ab9947239dd05b48838900fade068ad4f474
Roscoe Hart <roscoewh@gmail.com>
2020-07-28 20:38:49 +1000

    Add remaining petals

--- src/components/petals.js
+++ src/components/petals.js
@@ -1,83 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import PETALS from "../paths/petals"
+
 const SIZE = 128
 const RES = 20
 
-const PETALS = {
-  clover: [
-    <>
-      <path d="m3,0 h2 v1 h1 v4 h-1 v1 h-1 v-1 h-2 v-1 h-1 v-2 h2 v-2 m4,6 h1 v1 h2 v1 h1 v2 h-2 v2 h-2 v-1 h-1 v-4 h1 z" />
-      <rect x={1} y={9} width={1} height={1} />
-      <rect x={10} y={2} width={1} height={1} />
-    </>,
-    <>
-      <path d="m6,4  h1 v-2 h1 v-1 h2 v2 h2 v2 h-1 v1 h-4 v-1 h-1 v-1 m-1,2 v1 h1 v1 h-1 v2 h-1 v1 h-2 v-2 h-2 v-2 h1 v-1 z" />
-      <rect x={2} y={1} width={1} height={1} />
-      <rect x={9} y={10} width={1} height={1} />
-    </>,
-  ],
-  daisy: [
-    <>
-      <path d="m0,4 h1 v-2 h2 v1 h1 v1 h1 v2 h-2 v-1 h-3 z" />
-      <path d="m6,3 h1 v-3 h1 v1 h2 v2 h-1 v1 h-1 v1 h-2 z" />
-      <path d="m7,6 h2 v1 h3 v1 h-1 v2 h-2 v-1 h-1 v-1 h-1 z" />
-      <path d="m4,7 h2 v2 h-1 v3 h-1 v-1 h-2 v-2 h1 v-1 h1 z" />
-    </>,
-    <>
-      <path d="m4,0 h1 v1 h1 v4 h-1 v-1 h-1 v-2 h-1 v-1 h1 z" />
-      <path d="m10,3 h1 v1 h1 v1 h-1 v1 h-4 v-1 h1 v-1 h2 z" />
-      <path d="m6,7 h1 v1 h1 v2 h1 v1 h-1 v1 h-1 v-1 h-1 z" />
-      <path d="m1,6 h4 v1 h-1 v1 h-2 v1 h-1 v-1 h-1 v-1 h1 z" />
-    </>,
-  ],
-  daffodil: [
-    <>
-      <path d="m3,0 h2 v1 h1 v4 h-2 v-1 h-1 z" />
-      <path d="m8,3 h4 v2 h-1 v1 h-4 v-2 h1 z" />
-      <path d="m1,6 h4 v2 h-1 v1 h-4 v-2 h1 z" />
-      <path d="m6,7 h2 v1 h1 v4 h-2 v-1 h-1 z" />
-    </>,
-    <>
-      <path d="m6,1 h1 v-1 h2 v3 h-1 v1 h-1 v1 h-1 z" />
-      <path d="m7,6 h4 v1 h1 v2 h-3 v-1 h-1 v-1 h-1 z" />
-      <path d="m5,7 h1 v4 h-1 v1 h-2 v-3 h1 v-1 h1 z" />
-      <path d="m0,3 h3 v1 h1 v1 h1 v1 h-4 v-1 h-1 z" />
-    </>,
-  ],
-  tulip: [
-    <>
-      <path d="m5,1 h1 v1 h1 v2 h1 v2 h-1 v-1 h-2 v1 h-1 v-2 h1 z" />c
-      <path d="m0,3 h2 v1 h1 v2 h1 v2 h1 v2 h-1 v-1 h-2 v-5 h-2 z" />
-      <path d="m5,7 h1 v1 h1 v-1 h1 v-1 h1 v-2 h1 v-1 h2 v1 h-2 v5 h-2 v1 h-2 v1 h-1 v-1 h1 v-2 h-1 z" />
-    </>,
-    <>
-      <path d="m1,4 h1 v5 h2 v1 h1 v1 h-1 v-1h-2 v-1 h-1 z" />
-      <path d="m4,2 h1 v2 h-1 v2 h1 v2 h1 v2 h-1 v-2 h-1 v-2 h-1 v-2 h1 z" />
-      <path d="m5,0 h1 v1 h1 v1 h1 v2 h1 v2 h-1 v1 h-1 v1 h-1 v-1 h1 v-1 h1 v-2 h-1 v-2 h-1 v-1 h-1 z" />
-      <path d="m10,4 h1 v5 h-1 v1 h-2 v1 h-2 v-1 h2 v-1 h2 z" />
-    </>,
-  ],
-  bubble: [
-    <>
-      <path d="m4,0 h4 v1 h2 v1 h1 v2 h1 v4 h-1 v2 h-1 v1 h-2 v-1 h2 v-2 h1 v-4 h-1 v-2 h-2 v-1 h-4 z" />
-      <path d="m4,1 h-2 v1 h-1 v2 h-1 v4 h1 v2 h1 v1 h2 v1 h4 v-1 h-4 v-1 h-2 v-2 h-1 v-4 h1 v-2 h2 z" />
-      <path d="m8,3 h1 v1 h1 v2 h-1 v-2 h-1 z" />
-    </>,
-  ],
-  dandilion: [
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M4 1H6V2H4V1ZM2 2H3V3H2V2ZM3 9H2V10H3V9ZM8 11V10H6V11H8ZM4 10H5V11H4V10ZM10 9H9V10H10V9ZM2 6H1V8H2V6ZM1 4H2V5H1V4ZM11 7H10V8H11V7ZM10 4H11V6H10V4ZM10 2H9V3H10V2ZM8 1H7V2H8V1Z"
-    />,
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M6 3H7V4H6V3ZM5 5H6V4H5V3H4V4H3V5H4V6H3V7H4V8H3V9H4V8H5V9H6V8H7V9H8V8H9V7H8V6H9V5H8V4H9V3H8V4H7V5H8V6H7V7H6V8H5V7H4V6H5V5ZM7 7H8V8H7V7ZM5 5V4H4V5H5Z"
-    />,
-  ],
-}
 export const KINDS = Object.keys(PETALS)
 
 const Petals = ({ color = "pink", color2 = "hotpink", kind = "clover" }) => {
---
+++ src/paths/petals.js
@@ -0,0 +1,289 @@
+import React from "react"
+
+export default {
+  clover: [
+    <>
+      <path d="m3,0 h2 v1 h1 v4 h-1 v1 h-1 v-1 h-2 v-1 h-1 v-2 h2 v-2 m4,6 h1 v1 h2 v1 h1 v2 h-2 v2 h-2 v-1 h-1 v-4 h1 z" />
+      <rect x={1} y={9} width={1} height={1} />
+      <rect x={10} y={2} width={1} height={1} />
+    </>,
+    <>
+      <path d="m6,4  h1 v-2 h1 v-1 h2 v2 h2 v2 h-1 v1 h-4 v-1 h-1 v-1 m-1,2 v1 h1 v1 h-1 v2 h-1 v1 h-2 v-2 h-2 v-2 h1 v-1 z" />
+      <rect x={2} y={1} width={1} height={1} />
+      <rect x={9} y={10} width={1} height={1} />
+    </>,
+  ],
+  daisy: [
+    <>
+      <path d="m0,4 h1 v-2 h2 v1 h1 v1 h1 v2 h-2 v-1 h-3 z" />
+      <path d="m6,3 h1 v-3 h1 v1 h2 v2 h-1 v1 h-1 v1 h-2 z" />
+      <path d="m7,6 h2 v1 h3 v1 h-1 v2 h-2 v-1 h-1 v-1 h-1 z" />
+      <path d="m4,7 h2 v2 h-1 v3 h-1 v-1 h-2 v-2 h1 v-1 h1 z" />
+    </>,
+    <>
+      <path d="m4,0 h1 v1 h1 v4 h-1 v-1 h-1 v-2 h-1 v-1 h1 z" />
+      <path d="m10,3 h1 v1 h1 v1 h-1 v1 h-4 v-1 h1 v-1 h2 z" />
+      <path d="m6,7 h1 v1 h1 v2 h1 v1 h-1 v1 h-1 v-1 h-1 z" />
+      <path d="m1,6 h4 v1 h-1 v1 h-2 v1 h-1 v-1 h-1 v-1 h1 z" />
+    </>,
+  ],
+  daffodil: [
+    <>
+      <path d="m3,0 h2 v1 h1 v4 h-2 v-1 h-1 z" />
+      <path d="m8,3 h4 v2 h-1 v1 h-4 v-2 h1 z" />
+      <path d="m1,6 h4 v2 h-1 v1 h-4 v-2 h1 z" />
+      <path d="m6,7 h2 v1 h1 v4 h-2 v-1 h-1 z" />
+    </>,
+    <>
+      <path d="m6,1 h1 v-1 h2 v3 h-1 v1 h-1 v1 h-1 z" />
+      <path d="m7,6 h4 v1 h1 v2 h-3 v-1 h-1 v-1 h-1 z" />
+      <path d="m5,7 h1 v4 h-1 v1 h-2 v-3 h1 v-1 h1 z" />
+      <path d="m0,3 h3 v1 h1 v1 h1 v1 h-4 v-1 h-1 z" />
+    </>,
+  ],
+  tulip: [
+    <>
+      <path d="m5,1 h1 v1 h1 v2 h1 v2 h-1 v-1 h-2 v1 h-1 v-2 h1 z" />c
+      <path d="m0,3 h2 v1 h1 v2 h1 v2 h1 v2 h-1 v-1 h-2 v-5 h-2 z" />
+      <path d="m5,7 h1 v1 h1 v-1 h1 v-1 h1 v-2 h1 v-1 h2 v1 h-2 v5 h-2 v1 h-2 v1 h-1 v-1 h1 v-2 h-1 z" />
+    </>,
+    <>
+      <path d="m1,4 h1 v5 h2 v1 h1 v1 h-1 v-1h-2 v-1 h-1 z" />
+      <path d="m4,2 h1 v2 h-1 v2 h1 v2 h1 v2 h-1 v-2 h-1 v-2 h-1 v-2 h1 z" />
+      <path d="m5,0 h1 v1 h1 v1 h1 v2 h1 v2 h-1 v1 h-1 v1 h-1 v-1 h1 v-1 h1 v-2 h-1 v-2 h-1 v-1 h-1 z" />
+      <path d="m10,4 h1 v5 h-1 v1 h-2 v1 h-2 v-1 h2 v-1 h2 z" />
+    </>,
+  ],
+  bubble: [
+    <>
+      <path d="m4,0 h4 v1 h2 v1 h1 v2 h1 v4 h-1 v2 h-1 v1 h-2 v-1 h2 v-2 h1 v-4 h-1 v-2 h-2 v-1 h-4 z" />
+      <path d="m4,1 h-2 v1 h-1 v2 h-1 v4 h1 v2 h1 v1 h2 v1 h4 v-1 h-4 v-1 h-2 v-2 h-1 v-4 h1 v-2 h2 z" />
+      <path d="m8,3 h1 v1 h1 v2 h-1 v-2 h-1 z" />
+    </>,
+  ],
+  dandilion: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4 1H6V2H4V1ZM2 2H3V3H2V2ZM3 9H2V10H3V9ZM8 11V10H6V11H8ZM4 10H5V11H4V10ZM10 9H9V10H10V9ZM2 6H1V8H2V6ZM1 4H2V5H1V4ZM11 7H10V8H11V7ZM10 4H11V6H10V4ZM10 2H9V3H10V2ZM8 1H7V2H8V1Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M6 3H7V4H6V3ZM5 5H6V4H5V3H4V4H3V5H4V6H3V7H4V8H3V9H4V8H5V9H6V8H7V9H8V8H9V7H8V6H9V5H8V4H9V3H8V4H7V5H8V6H7V7H6V8H5V7H4V6H5V5ZM7 7H8V8H7V7ZM5 5V4H4V5H5Z"
+    />,
+  ],
+  zinnia: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 10V2H11V1H10V0H2V1H1V2H0V10H1V11H2V12H10V11H11V10H12ZM5 7V5H7V7H5Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M1 1L2 1V2L1 2V1ZM11 1V2H10V1H11ZM11 11V10H10V11H11ZM2 10V11H1V10H2ZM9 4V8H8V9H4L4 8H3V4H4V3L8 3V4H9ZM5 5V7H7V5H5Z"
+    />,
+  ],
+  rose: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9 3H8V5H11V7H7V5V2H8V1H9V3ZM3 2H5V4H3V2ZM1 5V7H4V9H3V11H4V10H5V7V5H1ZM9 10V8H7V10H9Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5 1H7V5H5H2V4H1V3H3V4H5V1ZM10 3H8V5H10V3ZM2 7V9H4V7H2ZM7 7H10V8H11V9H9V8H7V11H5V7H7Z"
+    />,
+  ],
+  propeller: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M6 0V1H5V0H6ZM5 1V4H6V5H4V4V1H5ZM8 6H7V4H8H11V5H8V6ZM11 5H12V6H11V5ZM6 8V7H8V8V11H7V8H6ZM7 11V12H6V11H7ZM1 6H0V7H1V8H4H5V6H4V7H1V6Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M6 4V5H7V4H8V1H7V0H6V1H5V4H6ZM1 7H4V6H5V5H4V4H1V5H0V6H1V7ZM11 5H8V6H7V7H8V8H11V7H12V6H11V5ZM6 8V7H5V8H4V11H5V12H6V11H7V8H6Z"
+    />,
+  ],
+  swirl: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4 2V1H3V2H2V3H1V7H2V8H3V9H6V8H7V7H6V8H3V7H2V3H3V2H4ZM9 4V3H6V4H5V5H6V4H9ZM10 5H9V4H10V5ZM10 9V5H11V9H10ZM9 10H10V9H9V10ZM9 10H8V11H9V10Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5 2V1H9V2H5ZM4 3V2H5V3H4ZM4 6H3V3H4V6ZM4 6H5V7H4V6ZM10 3H9V2H10V3ZM10 3H11V4H10V3ZM7 9V10H3V9H2V8H1V9H2V10H3V11H7V10H8V9H9V6H8V5H7V6H8V9H7Z"
+    />,
+  ],
+  vortex: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8 0V1H6V2H5V3H4V4H5V6H4V5H3V2H4V1H5V0H6H8ZM8 1H9V2H8V1ZM10 3V4H11V5H12V6V8H11V6H10V5H9V4H8V5H6V4H7V3H10ZM11 8V9H10V8H11ZM8 6H7V8H8V9H7V10H6V11H4V10H3V11H4V12H7V11H8V10H9V9V7H8V6ZM3 7V8H4V7H6V8H5V9H2V8H1V7H0V6V4H1V6H2V7H3ZM2 4H1V3H2V4Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M1 2L1 1H2V2H1ZM2 2H3V5H4V6H5V7H3L3 6H2L2 5L2 2ZM10 1V2L6 2V3H5V4V5H6V4H7V3H10V2L11 2V1L10 1ZM7 9L7 8V7H6V8H5V9H2V10H1L1 11H2L2 10H6V9H7ZM7 6V5L9 5V6H10V7V10H9V7L8 7V6H7ZM10 10H11V11H10V10Z"
+    />,
+  ],
+  frilly: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3 2V1H2H1V3V4H2V5H4V6H5V4H4H3V3H2V2H3ZM11 2V3H10V2H9V3H8V4V5H6V4H7V2H8V1H9H11V2ZM8 8H7V6H8V7H10V8H11V9V11H10H9V10H10V9H9V8H8ZM5 7H4V9H3V10H2V9H1V11H2H3H4V10H5V8H6V7H5Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5 0V1H4L4 4H5V5H6V3H5V2H6V0L5 0ZM8 4V5L7 5V6L9 6V5H10V6H12V5H11V4H8ZM6 7H7L7 8H8V11H7V12H6V10H7V9H6L6 7ZM4 7H5V6H4H3V7H2V6H0L0 7H1V8H3H4V7Z"
+    />,
+  ],
+  shuriken: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4 4V3H3V4H2V5H1V6H0V9H1V8H2V7L3 7V6H4V7H5L5 4H4ZM7 8H8V9H9V8H10V7H11V6H12V3H11V4H10V5H9V6H8L8 5H7L7 8Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3 1H4V2H5V3H6V4H5V5H8V4H9V3H8V2H7V1H6V0H3V1ZM7 7H4V8H3V9H4V10H5V11H6V12H9V11H8V10H7V9H6V8H7V7Z"
+    />,
+  ],
+  disc: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4 0H8V1H10V2H11V4H12V8H11V10H10V11H8V12H4V11H2V10H1V8H0V4H1V2H2V1H4V0ZM7 5H5V7H7V5Z"
+    />,
+    <>
+      <path d="M4 0H8V1H4V0Z" />
+      <path d="M2 2V1H4V2H2Z" />
+      <path d="M1 4V2H2V4H1Z" />
+      <path d="M1 8H0V4H1V8Z" />
+      <path d="M2 10H1V8H2V10Z" />
+      <path d="M4 11H2V10H4V11Z" />
+      <path d="M8 11V12H4V11H8Z" />
+      <path d="M10 10V11H8V10H10Z" />
+      <path d="M11 8V10H10V8H11Z" />
+      <path d="M11 4V8H12V4H11Z" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10 2H8V1H10V2ZM10 2H11V4H10V2Z"
+      />
+    </>,
+  ],
+  whispy: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M1 4V3H0V4H1ZM11 3V4H10V3H11ZM11 5V6H10V5H11ZM9 11V10H8V11H9ZM5 10V11H4V10H5Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9 1V0H7V1H6V3H7V5H8V3H7V1H9ZM9 1V3H10V1H9ZM9 8V7H8V6H7V7H8V8H9ZM11 8V9H9V8H11ZM11 8V6H12V8H11ZM6 9V7H5V9H6ZM6 9H7H8V10H7H6V9ZM6 4V5H5V4H6ZM4 2H5V4H4V2ZM3 2V1H4V2H3ZM2 4V2H3V4H2ZM2 4H1V5H2V4ZM3 6V5H5V6H3ZM2 7V6H3V7H2ZM2 7H1V9H2V10H4V9H2V7Z"
+    />,
+  ],
+  spiral: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4 0H9V1H4V0ZM3 2V1H4V2H3ZM2 3V2H3V3H2ZM2 7H1V3H2V7ZM3 8H2V7H3V8ZM4 9H3V8H4V9ZM8 9V10H4V9H8ZM9 8V9H8V8H9ZM9 4V8H10V4H9ZM8 3H9V4H8V3ZM5 3V2H8V3H5ZM5 4V3H4V4H3V6H4V7H5V8H7V7H8V5H7V4H5ZM5 4H4V6H5V7H7V5H5V4ZM10 2H9V1H10V2ZM11 3H10V2H11V3ZM11 9V3H12V9H11ZM10 10V9H11V10H10ZM9 11V10H10V11H9ZM3 11H9V12H3V11ZM2 10H3V11H2V10ZM1 9H2V10H1V9ZM1 9H0V8H1V9Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3 1H4V2H3V1ZM3 2V3H2V2H3ZM5 3H4V4H5V3ZM8 3H9V4H8V3ZM10 1H9V2H10V3H11V2H10V1ZM11 9H10V10H9V11H10V10H11V9ZM9 8H8V9H9V8ZM3 8H4V9H3V8ZM3 8H2V7H3V8ZM4 6H5V7H4V6ZM3 10H2V9H1V10H2V11H3V10Z"
+    />,
+  ],
+  taijitu: [
+    <>
+      <rect x="1" y="7" width="2" height="2" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 0V1H2V2H1V4H2V5H3H4V6H5V5H4V4L8 4V5H9V6H11V5H12V3H11V2L10 2V1L8 1V0H4ZM11 3V5H9V3H10L11 3Z"
+      />
+    </>,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M11 3H9V5H11V3ZM3 6H1V7H0V9H1V10H2V11H4V12H8V11H10V10H11V8H10V7H9H8V6H7V7H8V8H4V7H3V6Z"
+    />,
+  ],
+  carnivorous: [
+    <>
+      <rect x="3" y="2" width="1" height="1" />
+      <rect x="2" y="3" width="1" height="2" />
+      <rect x="1" y="5" width="1" height="2" />
+      <rect x="2" y="8" width="1" height="1" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8 2V1H10V2L11 2V4H12L12 8H11V9V10H10V11H9V12H7V11H6L6 9H7V8H6V7H7L7 5H6L6 4H7V3V2H8ZM9 3H10V5H9V4V3ZM10 7L10 5H11V7H10ZM9 9V7H10V9H9ZM9 9V10H8V9H9Z"
+      />
+    </>,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5 1V0L3 0V1H2V2H1V4H0V8H1V9V10H2V11H4V10H5V9V8H6L6 7H5V5H6V4H5V3H6V1L5 1ZM9 9V10H8V9H9ZM10 7V8V9H9V8V7H10ZM10 5H11V7H10V5ZM10 5H9V3H10V5Z"
+    />,
+  ],
+  borromean: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5 2V3H3V2H5ZM3 5H2V3H3V5ZM5 5V6H3V5H5ZM5 5V4H6V5H5ZM6 7V8H5V7H6ZM6 10V8H7V10H6ZM4 10H6V11H4V10ZM4 10V9H3V10H4ZM10 4H11V6H10V4ZM8 4H10V3H8V4ZM8 4V6H7V4H8ZM10 6V7H9V6H10Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8 1V2H6V1H8ZM6 4H5V2H6V4ZM6 4H7V5H6V4ZM8 2V3H9V2H8ZM3 5V6H2V5H3ZM2 8H1V6H2V8ZM4 8V9H2V8H4ZM4 8V6H5V8H4ZM9 10V9H10V7H9V6H7V7H6V8H7V7H9V9H7V10H9Z"
+    />,
+  ],
+  lollipop: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M7 0H6V1H5V3H6V4H4V3H3V1H2V2H1V4H2V5H3H4V6H3V7H1V6H0V8H1V9H3V8H4V6H5V5H6V4H7V2H8V0H7ZM5 12H6V11H7V9H6V8H8V9H9V11H10V10H11V8H10V7H9H8V6H9V5H11V6H12V4H11V3H9V4H8V6H7V7H6V8H5V10H4V12H5Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M6 0H4V1H3V3H4V4H6V5H7V6H8V7H10V8H12V7V6H11V5H9V6H8V4H9V3H11V2H10V1H8V2H7V3V4H6V3H5V1H6V0ZM8 9V8H6V7H5V6H4V5H2V4H0V5V6H1V7H3V6H4V8H3V9H1V10H2V11H4V10H5V9V8H6V9H7V11H6V12H8V11H9V9H8Z"
+    />,
+  ],
+  petunia: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9 2H7V4H6V5H7V6H9V5H10V3H9V2ZM6 7V8H5V10H6V11H8V10H9V9H8V8H7V7H6ZM2 4H1V6H2V7H3V6H4H5V4H4V3H2V4Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4 3V4H5V5H6V4H7V2H6V1H4V2H3V3H4ZM9 6V5H10V6H11V8H10V9H8V8H7V6H9ZM3 9V10H5V8H6V7H5V6H3V7H2V9H3Z"
+    />,
+  ],
+  whirligig: [
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M11 1H12V0H11V1ZM0 1H1H2V2H3L3 3H4V5H5V8H4H3H2V7H1V6H0L0 1ZM7 4H8H9H10L10 5L11 5V6H12V11H11H10V10H9V9H8V7H7V4ZM1 12H0L0 11H1L1 12Z"
+    />,
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0 0V1H1V0H0ZM5 7H8V8V9V10H7V11H6V12H1V11V10H2V9H3V8H5V7ZM11 12V11H12V12H11ZM7 5H4V4V3V2H5V1H6V0H11V1V2H10V3H9V4H7V5Z"
+    />,
+  ],
+}
