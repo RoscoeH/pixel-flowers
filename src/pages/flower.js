cdc122aef9a1897d6ea8778c7caadeb84bfe6d38
Roscoe Hart <roscoewh@gmail.com>
2020-08-08 19:10:00 +1000

    Improve edit menus

--- src/pages/flower.js
+++ src/pages/flower.js
@@ -3,6 +3,7 @@ import { jsx } from "theme-ui"
 import React, { useEffect, useState } from "react"
 import { Router } from "@reach/router"
 import { TinyColor } from "@ctrl/tinycolor"
+import { Global } from "@emotion/core"
 import { ThemeProvider } from "theme-ui"
 import { CaretLeft, CaretRight } from "react-bytesize-icons"
 
@@ -29,21 +30,47 @@ const Page = ({ id }) => {
   const { flower, setFlower, prevKind, nextKind } = useFlower(urlDecode(id))
   const [menu, setMenu] = useState(initMenu())
 
+  const collapseMenu = () => {
+    const collapsedMenu = menu
+    Object.keys(menu).forEach(submenu => {
+      collapsedMenu[submenu] = {
+        ...menu[submenu],
+        open: false,
+      }
+    })
+    setMenu(collapsedMenu)
+  }
+
   const toggleMenu = part => () => {
-    console.log("set menu " + part)
+    const open = !menu[part].open
+    collapseMenu()
     setMenu({
       ...menu,
-      [part]: { ...menu[part], open: !menu[part].open },
+      [part]: { ...menu[part], open },
+    })
+  }
+
+  const setColor = (part, colorNumber) => ({ target }) => {
+    setFlower({
+      ...flower,
+      [`${part}Color${colorNumber || ""}`]: target.value,
     })
   }
 
   return (
     <ThemeProvider
       theme={{
-        colors: { background: flower ? flower.backgroundColor : "blue" },
+        colors: { background: flower.backgroundColor },
       }}
     >
-      <div sx={{ height: "100%", bg: "background" }}>
+      <Global
+        styles={theme => ({
+          body: {
+            backgroundColor: theme.colors.background,
+          },
+        })}
+      />
+      <div sx={{ height: "100%" }}>
         <SEO title="Flower" />
         <FlowerSvg width="100%" height={320} {...flower} />
         <PartMenu>
@@ -53,8 +80,12 @@ const Page = ({ id }) => {
               name={part}
               kind={flower[part]}
               onClick={toggleMenu(part)}
+              onSetColor={setColor(part)}
+              onSetColor2={setColor(part, 2)}
               onPrev={() => prevKind(part)}
               onNext={() => nextKind(part)}
+              color={flower[`${part}Color`]}
+              color2={flower[`${part}Color2`]}
               open={menu[part].open}
             />
           ))}
@@ -68,12 +99,16 @@ const PartMenu = ({ children }) => (
   <div sx={{ width: "100%", px: 3 }}>{children}</div>
 )
 const PartMenuItem = ({
+  open,
   name,
   kind,
-  open,
+  color,
+  color2,
   onClick,
   onPrev,
   onNext,
+  onSetColor,
+  onSetColor2,
   ...props
 }) => {
   return (
@@ -84,6 +119,9 @@ const PartMenuItem = ({
         "&:not(:last-child)": {
           mb: 2,
         },
+        "& > *:not(:last-child)": {
+          mb: 2,
+        },
       }}
       {...props}
     >
@@ -92,41 +130,73 @@ const PartMenuItem = ({
           fontSize: 4,
           textTransform: "uppercase",
           color: "text",
-          "&:not(:last-child)": { pb: 2 },
         }}
         onClick={onClick}
       >
         {name}
       </div>
       {open && (
-        <div
-          sx={{
-            display: "flex",
-            border: "1px solid muted",
-            borderColor: "muted",
-            "& > *:not(:last-child)": {
-              mr: 2,
-            },
-          }}
-        >
-          <Button onClick={onPrev}>
-            <CaretLeft sx={{ display: "block" }} />
-          </Button>
-          <span
+        <React.Fragment>
+          <div
+            sx={{
+              display: "flex",
+              "& > *:not(:last-child)": {
+                mr: 2,
+              },
+            }}
+          >
+            <Button onClick={onPrev}>
+              <CaretLeft width={24} height={24} sx={{ display: "block" }} />
+            </Button>
+            <span
+              sx={{
+                bg: "muted",
+                flex: "1 1 auto",
+                textAlign: "center",
+                fontSize: 3,
+                lineHeight: 2,
+              }}
+            >
+              {kind}
+            </span>
+            <Button onClick={onNext}>
+              <CaretRight width={24} height={24} sx={{ display: "block" }} />
+            </Button>
+          </div>
+
+          <input
             sx={{
               bg: "muted",
               flex: "1 1 auto",
               textAlign: "center",
               fontSize: 3,
               lineHeight: 2,
+              border: "none",
+              bg: color,
+              color: new TinyColor(color).isDark() ? "background" : "text",
+              width: "100%",
             }}
-          >
-            {kind}
-          </span>
-          <Button onClick={onNext}>
-            <CaretRight sx={{ display: "block" }} />
-          </Button>
-        </div>
+            value={color}
+            onChange={onSetColor}
+          />
+          {color2 && (
+            <input
+              sx={{
+                bg: "muted",
+                flex: "1 1 auto",
+                textAlign: "center",
+                fontSize: 3,
+                lineHeight: 2,
+                border: "none",
+                bg: color2,
+                color: new TinyColor(color).isDark() ? "background" : "text",
+                width: "100%",
+              }}
+              value={color2}
+              onChange={onSetColor2}
+            />
+          )}
+        </React.Fragment>
       )}
     </div>
   )
