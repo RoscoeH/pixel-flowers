fe4c29cce17c39db5505d39885e5838d72fbc92d
Roscoe Hart <roscoewh@gmail.com>
2021-05-02 16:48:02 +1000

    Add typeface-work-sans

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,17 +1,24 @@
+import "typeface-work-sans"
+
 export default {
+  fonts: {
+    body: '"Work Sans", sans-serif',
+    heading: '"Work Sans", sans-serif',
+    monospace: "Menlo, monospace",
+  },
   sizes: [0, 4, 8, 12, 16, 24, 32, 48, 64, 128, 256],
   space: [0, 4, 8, 12, 16, 24, 32, 64, 48, 128, 256],
-
   radii: [0, 2, 4, 8, 16, 24, 32, 64, 99999],
   borders: {
     default: "1px solid",
   },
   colors: {
     light: "#ffffff",
-    dark: "#000000",
+    dark: "#2e1c43",
     text: "rgba(0,0,0,0.7)",
     background: "#fff",
     primary: "#9b5de5",
     muted: "#bea6da",
+    hover: "#6f29c2",
   },
 }
