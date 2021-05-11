341c4e90ef18ce69eb039f9b2afdeac8a89a6db0
Roscoe Hart <roscoewh@gmail.com>
2021-05-11 20:20:48 +1000

    Fix Gatsby prerender issue

--- src/components/RandomFlower.js
+++ src/components/RandomFlower.js
@@ -1,11 +1,17 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from "theme-ui"
+import { useEffect } from "react"
 
 import { useFlower, pickRandomFlower } from "../hooks/useFlower"
 import { FlowerSvg as Flower } from "./Flower"
 
 export default function RandomFlower({ size }) {
-  const { flower } = useFlower(pickRandomFlower())
+  const { flower, setFlower } = useFlower(pickRandomFlower())
+
+  useEffect(() => {
+    setFlower(pickRandomFlower())
+  }, [])
+
   return <Flower {...flower} width={size} height={size} rounded />
 }
--- src/components/RandomFlowers.js
+++ src/components/RandomFlowers.js
@@ -1,15 +1,11 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { useState } from "react"
 
 import { range } from "../core/utils"
-import { pickRandomFlower } from "../hooks/useFlower"
-// import RandomFlower from "./RandomFlower"
-import { FlowerSvg as Flower } from "./Flower"
+import RandomFlower from "./RandomFlower"
 
 export default function RandomFlowers({ count = 60, size = 96 }) {
-  const [flowers] = useState(range(count).map(pickRandomFlower))
   return (
     <div
       sx={{
@@ -20,8 +16,8 @@ export default function RandomFlowers({ count = 60, size = 96 }) {
         margin: "0 auto",
       }}
     >
-      {flowers.map((flower, i) => (
-        <Flower key={i} {...flower} width={size} height={size} rounded />
+      {range(count).map(i => (
+        <RandomFlower key={i} size={size} />
       ))}
     </div>
   )
